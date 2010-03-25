d80e6428e8b643cbec9aea2b2c64a365c9613373
wycats <wycats@gmail.com>
2010-03-24 18:54:44 -0700

    Add comments

--- lib/artifice.rb
+++ lib/artifice.rb
@@ -39,6 +39,8 @@ private
   end
 
   module Net
+    # This is an internal object that can receive Rack requests
+    # to the application using the Rack::Test API
     class RackRequest
       include Rack::Test::Methods
       attr_reader :app
@@ -53,6 +55,9 @@ private
         attr_accessor :endpoint
       end
 
+      # Net::HTTP uses a @newimpl instance variable to decide whether
+      # to use a legacy implementation. Since we are subclassing
+      # Net::HTTP, we must set it
       @newimpl = true
 
       # We don't need to connect, so blank out this method
@@ -68,6 +73,7 @@ private
       # @param [optional, String, #read] body This should
       #   be sent as "rack.input". If it's a String, it will
       #   be converted to a StringIO.
+      # @return [Net::HTTPResponse]
       #
       # @yield [Net::HTTPResponse] If a block is provided,
       #   this method will yield the Net::HTTPResponse to
@@ -90,6 +96,15 @@ private
 
     private
 
+      # This method takes a Rack response and creates a Net::HTTPResponse
+      # Instead of trying to mock HTTPResponse directly, we just convert
+      # the Rack response into a String that looks like a normal HTTP
+      # response and call Net::HTTPResponse.read_new
+      #
+      # @param [Array(#to_i, Hash, #each)] response a Rack response
+      # @return [Net::HTTPResponse]
+      # @yield [Net::HTTPResponse] If a block is provided, yield the
+      #   response to it after the body is read
       def make_net_http_response(response)
         status, headers, body = response.status, response.headers, response.body
 
