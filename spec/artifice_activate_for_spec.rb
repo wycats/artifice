e91c1d85feb944a58e98687fabe5ab548d4c1d8c
Austin Ziegler <austin@halostatue.ca>
2011-09-23 22:37:09 -0400

    Minimal tests that show activate_for working.

--- spec/artifice_activate_for_spec.rb
+++ spec/artifice_activate_for_spec.rb
@@ -95,20 +95,6 @@ describe "Artifice#activate_for" do
     replace_tcpsocket(tcp_socket)
   end
 
-  post_with_block = {
-    :request_headers  =>
-      [ "%s /index HTTP/1.1", "Accept: */*",
-        "Content-Type: application/x-www-form-urlencoded",
-        "Content-Length: 7", "Host: %s", "", "" ].join("\r\n"),
-    :request_body     => "foo=bar",
-    :response         =>
-      [ "HTTP/1.1 200 OK", "Content-Type: text/html",
-        "X-Test-Method: %s", "X-Test-Input: %s",
-        "X-Test-Scheme: %s", "X-Test-Host: %s",
-        "X-Test-Port: %s",
-        "Content-Length: 11", "", "Hello world" ].join("\n"),
-  }
-
   post_without_block = {
     :request_headers  =>
       [ "%s /index HTTP/1.1", "Accept: */*",
@@ -138,14 +124,9 @@ describe "Artifice#activate_for" do
   }
 
   Data = {
-    :post_with_block    => post_with_block,
     :post_without_block => post_without_block,
     :get                => get,
   }
-
-  DoubleData = [
-    [ 16384, "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 11\n\nHello world" ],
-  ]
 
   require "artifice"
 
@@ -170,21 +151,7 @@ describe "Artifice#activate_for" do
       ::Net::HTTP.should_not == NET_HTTP
     end
 
-    describe "and making a POST request to google.ca with Net::HTTP.start {}" do
-      before do
-        mock_socket(:post_with_block, "POST", "google.ca")
-
-        @response = Net::HTTP.start("google.ca", 80) do |http|
-          http.post("/index", "foo=bar")
-        end
-      end
-
-#     it_should_behave_like "a working POST request to google.ca"
-#     it_should_behave_like "a working HTTP request to google.ca"
-    end
-
-    describe "and making a POST request to google.ca with Net::HTTP.start" do
-
+    describe "and making a POST request to google.ca with Net::HTTP.new" do
       before do
         mock_socket(:post_without_block, "POST", "google.ca", 443, "https")
 
@@ -199,7 +166,7 @@ describe "Artifice#activate_for" do
       it_should_behave_like "a working HTTPS request to google.ca"
     end
 
-    describe "and make a GET request with Net::HTTP.start" do
+    describe "and make a GET request with Net::HTTP.new" do
       before do
         mock_socket(:get, "GET", "google.ca")
 
@@ -210,67 +177,5 @@ describe "Artifice#activate_for" do
       it_should_behave_like "a working GET request to google.ca"
       it_should_behave_like "a working HTTP request to google.ca"
     end
-
-#   describe "and make a GET request with Net::HTTP.get_response" do
-#     before do
-#       mock_socket(:get, "GET", "google.ca")
-    #       no connection: close
-#       @response = Net::HTTP.get_response(URI.parse("http://google.ca/get"))
-#     end
-
-#     it_should_behave_like "a working GET request to google.ca"
-#     it_should_behave_like "a working HTTP request to google.ca"
-#   end
-
-#   describe "and make a GET request with Net::HTTP::Get.new" do
-#     before do
-#       Net::HTTP.start('google.ca') do |http|
-#         req = Net::HTTP::Get.new('/get')
-#         @response = http.request(req)
-#       end
-#     end
-
-#     it_should_behave_like "a working GET request to google.ca"
-#     it_should_behave_like "a working HTTP request to google.ca"
-#   end
-#   
-#   describe "and make a POST request with Net::HTTP::Post.new" do
-#     before do
-#       Net::HTTP.start('google.ca') do |http|
-#         req = Net::HTTP::Post.new('/index')
-#         req.body = 'foo=bar'
-#         @response = http.request(req)
-#       end
-#     end
-
-#     it_should_behave_like "a working POST request to google.ca"
-#     it_should_behave_like "a working HTTP request to google.ca"
-#   end
   end
-
-# describe "when activating with a block" do
-#   
-#   before do
-#     ::Net::HTTP.should == NET_HTTP
-#   end
-
-#   after do
-#     ::Net::HTTP.should == NET_HTTP
-#   end
-
-#   it "deactivates automatically after the block is executed" do
-#     Artifice.activate_with( lambda {} ) do
-#       ::Net::HTTP.should == Artifice::Net::HTTP
-#     end
-#   end
-
-#   it "deactivates even if an exception is raised from within the block" do
-#     lambda {
-#       Artifice.activate_with( lambda {} ) do
-#         ::Net::HTTP.should == Artifice::Net::HTTP
-#         raise 'Boom!'
-#       end
-#     }.should raise_error
-#   end
-# end
 end
